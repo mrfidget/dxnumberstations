0cf59aab479bfdc879b2be353d9793c31d4c9e78
mrfidget <ferdie@ferdie.co.uk>
2020-04-22 21:36:58 +0100

    #3 Add 2 switches for the time range #3 Add a default time range

--- dxMain.ps1
+++ dxMain.ps1
@@ -1,18 +1,22 @@
-# initial commit
+
+param (
+    [datetime]$timemin=(Get-Date),
+    [datetime]$timemax=((Get-Date).AddMinutes(30))
+)
+
+
 function get-data-from-pryiom($timemin,$timemax) {
-#connect to priyom and get a json file based on the time interval
-    $url = "http://calendar.priyom.org/events?timeMin=$timemin&timeMax=$timemax";
+#connect to priyom and get a json file based on the time interval, by default read within 30 minutes of now
+    $time_min=$timemin.ToString('yyyy-MM-ddTHH:mm:00.000Z');
+    $time_max=$timemax.ToString('yyyy-MM-ddTHH:mm:00.000Z');
+    $url = "http://calendar.priyom.org/events?timeMin=$time_min&timeMax=$time_max";
     $request_headers = @{'Host' = 'calendar.priyom.org'; 'Origin' = 'http://priyom.org'; 'Referer' = 'http://priyom.org/' };
     $response = Invoke-WebRequest -Uri $url -Headers $request_headers -UseBasicParsing | ConvertFrom-JSON;
     return $response;
 }
-$timemin='2020-04-22T10:00:00.000Z';
-$timemax='2020-04-22T10:30:00.000Z';
 
 $response = get-data-from-pryiom $timemin $timemax;
 
 foreach ($station in $response.items) {
     $station.summary + ' -> ' + $station.start.dateTime;
 }
-
-
