eb3f5ea9292541bc3278ba64e9cf44f20c267cc3
mrfidget <ferdie@ferdie.co.uk>
2020-04-28 10:58:45 +0100

    #5 convert time to UTC #4 clean up and split in functions

--- dxMain.ps1
+++ dxMain.ps1
@@ -1,17 +1,35 @@
-
 param (
+# if no param are given, default read within 30 minutes of now
     [datetime]$timemin=(Get-Date),
     [datetime]$timemax=((Get-Date).AddMinutes(30))
 )
+function convert-to-UTC($atime){
+    return $atime.ToUniversalTime();
+}
 
+function convert-time-format($atime){
+    $atime=$atime.ToString('yyyy-MM-ddTHH:mm:00.000Z');
+    return $atime;
+}
 
+function build-headers(){
+    return @{'Host' = 'calendar.priyom.org'; 'Origin' = 'http://priyom.org'; 'Referer' = 'http://priyom.org/' };
+}
 function get-data-from-pryiom($timemin,$timemax) {
-#connect to priyom and get a json file based on the time interval, by default read within 30 minutes of now
-    $time_min=$timemin.ToString('yyyy-MM-ddTHH:mm:00.000Z');
-    $time_max=$timemax.ToString('yyyy-MM-ddTHH:mm:00.000Z');
+#convert the time to UTC
+#give them the appropriate format
+#suppress progress bar with preferences
+#connect to priyom and get a json file based on the time interval
+#convert the response to a handy PSObject
+    $timemin=convert-to-UTC($timemin);
+    $timemax=convert-to-UTC($timemax);
+    $time_min=convert-time-format($timemin);
+    $time_max=convert-time-format($timemax);
     $url = "http://calendar.priyom.org/events?timeMin=$time_min&timeMax=$time_max";
-    $request_headers = @{'Host' = 'calendar.priyom.org'; 'Origin' = 'http://priyom.org'; 'Referer' = 'http://priyom.org/' };
-    $response = Invoke-WebRequest -Uri $url -Headers $request_headers -UseBasicParsing | ConvertFrom-JSON;
+    $progressPreference = 'silentlyContinue';
+    $request_headers = build-headers;
+    $response = Invoke-WebRequest -Uri $url -Headers $request_headers -UseBasicParsing -ErrorAction Stop | ConvertFrom-JSON;
+    $progressPreference = 'Continue';
     return $response;
 }
 
